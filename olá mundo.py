6306186b7f31e8f91c54431b5f72a3072385ba8c
mariaestella2006 <mariah.estella15@gmail.com>
2024-06-06 14:13:26 -0300

    primeiro envio: apresentação

---
+++ olá mundo.py
@@ -0,0 +1,5 @@
+nome="Maria Estella"
+idade= "18"
+cidade="Mogi das Cruzes"
+formação= "estudande de biomedicina e estou estudando python"
+print(f"Olá meu nome é: {nome}, tenho {idade} anos, atualmente moro em {cidade} e sou {formação}") 
